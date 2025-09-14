9111bd64f81e910e2a45ba091805bb66573462cb
Rakshi2609 <rakshithganjimut@gmail.com>
2025-09-14 10:51:25 +0530

    Exaust

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use(helmet({
 app.use(cors({
     origin: function (origin, callback) {
         const allowedOrigins = [
-            
+            process.env.FRONTEND_URL,
             'http://localhost:5173',
             'http://localhost:5174',
             'http://localhost:5175'
