206c004e031c44c6247bee579b4db219e89543e4
Rakshi2609 <rakshithganjimut@gmail.com>
2025-09-14 10:27:28 +0530

    3rd phase

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,7 @@ app.use(helmet({
 app.use(cors({
     origin: function (origin, callback) {
         const allowedOrigins = [
+            
             'http://localhost:5173',
             'http://localhost:5174',
             'http://localhost:5175'
